7b877c7d90b5ac04e08c25eca8735657dc48c13a
出入平安 <921227965@qq.com>
2017-05-12 15:04:03 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
 
-module.exports = {
+module.exports = { 
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
